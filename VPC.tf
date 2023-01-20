d0113a641ea2575ae2a0375c776845df3ef13821
Belev-va <belev.job@gmail.com>
2023-01-20 15:23:44 +0400

    update

--- VPC.tf
+++ VPC.tf
@@ -11,7 +11,6 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-# Create VPC
 resource "aws_vpc" "main" {
  cidr_block = "10.0.0.0/16"
  
@@ -26,6 +25,7 @@ resource "aws_vpc" "main" {
  cidr_block = "10.0.1.0/24"
  vpc_id            = "${aws_vpc.main.id}"
  availability_zone = "eu-central-1a"
+ map_public_ip_on_launch = "true"
  
  tags = {
     Name            = "how_light_public_1"
@@ -36,6 +36,7 @@ resource "aws_vpc" "main" {
  cidr_block = "10.0.2.0/24"
  vpc_id            = "${aws_vpc.main.id}"
  availability_zone = "eu-central-1b"
+ map_public_ip_on_launch = "true"
  
  tags = {
     Name            = "how_light_public_2"
@@ -48,8 +49,7 @@ resource "aws_subnet" "aws-subnet-private_1" {
  cidr_block = "10.0.3.0/24"
  vpc_id            = "${aws_vpc.main.id}"
  availability_zone = "eu-central-1a"
- map_public_ip_on_launch = "true"
-
+ 
  tags = {
     Name            = "how_light_private_1"
   }
@@ -59,25 +59,25 @@ resource "aws_subnet" "aws-subnet-private_1" {
  cidr_block = "10.0.4.0/24"
  vpc_id            = "${aws_vpc.main.id}"
  availability_zone = "eu-central-1b"
- map_public_ip_on_launch = "true"
-
+ 
  tags = {
     Name            = "how_light_private_2"
   }
  }
-
-# Add AWS internet gateway
-resource "aws_internet_gateway" "how_light_internet_gateway" {
-  vpc_id = "${aws_vpc.main.id}"
-  tags = {
-    Name            = "internet-gateway"
-  }
+ 
+ # Add AWS internet gateway
+ resource "aws_internet_gateway" "how_light_internet_gateway" {
+    vpc_id = "${aws_vpc.main.id}"
+    tags = {
+        Name            = "internet-gateway"
+    }
 }
 
 resource "aws_network_acl" "acl-private" {
   vpc_id     = aws_vpc.main.id
   subnet_ids = [aws_subnet.aws-subnet-private_1.id, aws_subnet.aws-subnet-private_2.id]
-
+  
+  
   ingress {
     cidr_block = "0.0.0.0/0"
     action    = "Deny"
@@ -85,7 +85,9 @@ resource "aws_network_acl" "acl-private" {
     to_port     = 0
     protocol    = "-1"
     rule_no   = 200
+
   }
+  
   egress {
     cidr_block = "0.0.0.0/0"
     action    = "Allow"
@@ -93,19 +95,17 @@ resource "aws_network_acl" "acl-private" {
     to_port     = 0
     protocol    = "-1"
     rule_no   = 200
+
   }
+  
+  
 
   tags = {
     Name = "acl-private"
   }
 }
 
-# Add variable
-#variable "allowed_ports" {
-#  description = "Allowed ports from/to host"
-#  type        = list
-#  default     = ["80", "443", "8080", "8443", "8000"]
-#}
+
 
 # Create security group
 resource "aws_security_group" "howlight-web-sg" {
@@ -131,14 +131,17 @@ resource "aws_security_group" "howlight-web-sg" {
     protocol    = "tcp"
     cidr_blocks = ["23.111.202.142/32", "23.111.123.20/32", "23.111.123.15/32", "23.111.123.23/32"]
   }
-
-  # allow egress traffic
+ 
+  # allow egress traffic 
   egress {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
   }
+}
 
-
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDEVAa67nfRw2GfyWknxhnkwtDeqcn4MtQ0xuWU8CmlGP01foGshsbqRln0Jk4kmmD9/jVxhPNa3ONz/eJ1nUsBNz5yoG79wuNkS9pFFECJUBgG5dylyByVupCYSo3ngjFw6yLXHWe2rIxn8MJeilycZW5jGwZtS/7E3N5f9N3wHgXTA1yFqUpwvu6DMubhzoFafKv/uwgaCmihogl3a9OxMHHzE/nWhDkAH8AzUsweA6pnjsnf4OR2G0QvOpOxbNIMVF++WUEEgE+YOg/Y5mBt61dxcOVlQGIsevgaYUL1dJQsBt70d8z1m1aSEweIGYIg+r3xzVjugC/0kXLElphJZZFiGKsk/BgxXLgep0j+HAn1CUQ3pNSLXejabW8oguYtxMoWP4OkLK2UjamfgI1b56zPKOYNVvh/VIkVlL1tQWiTOX/HBqGoJoNu4+XTB/SqZD2j1yM3DdhgPdS2AIiDVbWefwPP82cUltTUcNTEZB2arB8GWFO1P+ts/zWoMOE= belev@DESKTOP-C9NNOPT"
 }
