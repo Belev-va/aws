437d93d64f7cecbcfc911f32a56362b08667fb6b
Belev-va <belev.job@gmail.com>
2023-01-24 00:29:52 +0400

    update

--- EC2.tf
+++ EC2.tf
@@ -16,7 +16,7 @@ EOF
 resource "aws_instance" "linux_minimal_2" {
   ami                    = "ami-0d500797138456fbb"
   instance_type          = "t2.micro"
-  subnet_id              = "${aws_subnet.aws-subnet-public_2.id}"
+  vpc_security_group_ids = [aws_security_group.howlight-web-sg.id]
   user_data = <<EOF
 #!/bin/bash
 sudo apt update
