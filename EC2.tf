878fa380f41a6c1deee32fe1f778ec5fb7538ddc
Belev-va <belev.job@gmail.com>
2023-01-23 23:52:14 +0400

    update

--- EC2.tf
+++ EC2.tf
@@ -13,7 +13,7 @@ EOF
 
 }
 
-resource "aws_instance" "linux_minimal" {
+resource "aws_instance" "linux_minimal_2" {
   ami                    = "ami-0d500797138456fbb"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.howlight-web-sg.id]
