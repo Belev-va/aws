0a135991945492082b690e038b673e731260312a
Belev-va <belev.job@gmail.com>
2023-01-25 17:25:11 +0400

    update

--- EC2.tf
+++ EC2.tf
@@ -27,6 +27,5 @@ myip=curl http://169.254.169.254/latest/meta-data/local-ipv4
 echo "<h2>WebServer with IP: $myip</h2><br>Build by Terraform!" > /var/www/html/index.html
 sudo service httpd start
 chkconfig httpd on
-echo "Hello World" > /var/www/html/index.html
 EOF
 }
