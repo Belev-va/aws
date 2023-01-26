46ff5ac6d7901ac825912615ee5cbf984d8ba67f
Belev-va <belev.job@gmail.com>
2023-01-26 21:56:08 +0400

    update

--- EC2.tf
+++ EC2.tf
@@ -25,7 +25,7 @@ resource "aws_instance" "test" {
 #!/bin/bash
 yum -y update
 yum -y install httpd
-myip='curl http://169.254.169.254/latest/meta-data/local-ipv4'
+myip=curl http://169.254.169.254/latest/meta-data/local-ipv4
 echo "<h2>WebServer with IP: $myip</h2><br>Build by Terraform!" > /var/www/html/index.html
 sudo service httpd start
 chkconfig httpd on
